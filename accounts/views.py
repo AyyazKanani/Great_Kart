daf58366034ec39aa3ecbf24e774fb72797b1ff2
Ayyaz Kanani <ayyazkanani@gmail.com>
2026-09-21 17:58:10 +0530

    Add dashboard: view with login_required, URLs, template, update navbar and login redirect

--- accounts/views.py
+++ accounts/views.py
@@ -56,11 +56,15 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             # messages.success(request, 'You are now logged in.')
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
     return render(request, 'accounts/login.html')
+
+@login_required(login_url = 'login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
 
 @login_required(login_url = 'login')
 def logout(request):
