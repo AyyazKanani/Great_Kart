72fe164d32fa09c125129ad3615c3faedbc18c5c
Ayyaz Kanani <ayyazkanani@gmail.com>
2026-09-23 13:35:11 +0530

    Implement place_order: save billing, calculate totals/tax, generate order number

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm
+from .models import Order
+import datetime
 
 # Create your views here.
 
@@ -14,12 +16,49 @@ def place_order(request, total=0, quantity=0):
     if cart_count <= 0:
         return redirect('store')
 
+    grand_total = 0
+    tax = 0
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-            # For now just create order logic will be added in next video
-            # This is the initial version as per transcript
-            pass
-        return HttpResponse('Place order - form valid check')
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            # Generate order number
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime("%Y%m%d")
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+
+            for item in cart_items:
+                total += (item.product.price * item.quantity)
+                quantity += item.quantity
+            tax = (2 * total)/100
+            grand_total = total + tax
+
+            data.order_total = grand_total
+            data.tax = tax
+            data.save()
+
+            return redirect('checkout')
+        else:
+            return redirect('checkout')
     else:
         return redirect('checkout')
